7b17828a1c6ecb68411d4a7b77a51b91cf66a4e8
mohanpatrick <121981571+mohanpatrick@users.noreply.github.com>
2025-04-19 16:21:32 -0700

    Update get_FCELIM_drafts_adp.R

--- get_FCELIM_drafts_adp.R
+++ get_FCELIM_drafts_adp.R
@@ -108,7 +108,7 @@ fwrite(mfl_leagues,"mfl_league_ids.csv",quote = TRUE)
 # FOR TESTING
 
 mfl_leagues <- mfl_leagues |>
-  slice_sample(n=30)
+  slice_sample(n=60)
 
 cli::cli_alert("Starting draft pull")
 cli::cli_alert(now())
