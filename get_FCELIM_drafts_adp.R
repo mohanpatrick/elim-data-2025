54d91ee5cea1edfbe776db498bbea067791bd015
mohanpatrick <121981571+mohanpatrick@users.noreply.github.com>
2025-04-26 12:39:36 -0700

    Add files via upload

--- get_FCELIM_drafts_adp.R
+++ get_FCELIM_drafts_adp.R
@@ -23,7 +23,7 @@ find_leagues = "TRUE"
 polite = "FALSE"
 search_string="zzz #FCEliminator"
 total_picks_in_draft = 288
-user ="MAIN"
+user ="COMMISH"
 # Exclude blind bid leagues
 leagues_to_exclude = c(19123,33163,39863,52021,64792,58866,10144,15472,33121,42972,44072,57215,59150,65052,69507, 41474)
 leagues_to_exclude_adp = c(15099,28530,29122,29276,37484,45539,50996,69507,70181,70715)
@@ -38,7 +38,8 @@ if ( user == "COMMISH") {
 
 
 #GITHUB_PAT <- Sys.getenv(c("GITHUB_PAT"))
-mfl_client <- Sys.getenv(c("COMMISH_CLIENT"))
+#mfl_client <- Sys.getenv(c("COMMISH_CLIENT"))
+mfl_client = "COMMISHMFLCLIENT"
 mfl_user_id <- Sys.getenv(c("MFL_COMMISH_USER_ID"))
 mfl_pass <- Sys.getenv(c("MFL_COMMISH_PWD"))
 cli::cli_alert("Client ID: {mfl_client}")
@@ -124,7 +125,7 @@ fwrite(mfl_leagues,"mfl_league_ids.csv",quote = TRUE)
 # FOR TESTING
 
 mfl_leagues <- mfl_leagues |>
-  slice_sample(n=60)
+  slice_sample(n=10)
 
 cli::cli_alert("Starting draft pull")
 cli::cli_alert(now())
