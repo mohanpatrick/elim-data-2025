edd8b34ad5c28733ab05bf7d05e635f0c519c814
mohanpatrick <121981571+mohanpatrick@users.noreply.github.com>
2025-04-26 17:39:25 -0700

    Update test_commish.R

--- test_commish.R
+++ test_commish.R
@@ -23,14 +23,14 @@ find_leagues = "TRUE"
 polite = "FALSE"
 search_string="zzz #FCEliminator"
 total_picks_in_draft = 288
-user ="COMMISH"
+user ="MAIN"
 # Exclude blind bid leagues
 leagues_to_exclude = c(19123,33163,39863,52021,64792,58866,10144,15472,33121,42972,44072,57215,59150,65052,69507, 41474)
 leagues_to_exclude_adp = c(15099,28530,29122,29276,37484,45539,50996,69507,70181,70715)
 #GITHUB_PAT <- Sys.getenv(c("GITHUB_PAT"))
-#mfl_client <- Sys.getenv(c("MFL_CLIENT"))
-#mfl_user_id <- Sys.getenv(c("MFL_USER_ID"))
-#mfl_pass <- Sys.getenv(c("MFL_PWD"))
+mfl_client <- Sys.getenv(c("MFL_CLIENT"))
+mfl_user_id <- Sys.getenv(c("MFL_USER_ID"))
+mfl_pass <- Sys.getenv(c("MFL_PWD"))
 
 
 if ( user == "COMMISH") {
